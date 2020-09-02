c00534419e310c8a12b3e28c5287e5b1992b80c0
Champani Udayangani <wcuhamy@outlook.com>
2020-09-02 11:39:36 +0530

    Added endpoint to post on page feed

--- routes/facebookAPI.js
+++ routes/facebookAPI.js
@@ -6,9 +6,11 @@ const router = Router();
    
 // });
 
+let pageID;
+let pageAccessToken;
+
 router.get("/pages", (req, res) => {
-    let pageID;
-    let pageAccessToken;
+   
     var options = {
         uri: 'https://graph.facebook.com/v2.8/me/accounts',
         headers: {
@@ -22,12 +24,42 @@ router.get("/pages", (req, res) => {
         .then(function (data) {
             pageID = data.id;
             pageAccessToken = data.access_token;
-            console.log('pageID ', pageID);
+            res.status(200).send(data);
         })
         .catch(function (err) {
-            console.log('Request failed..')
+            res.status(400).send(err);
         });
 });
+
+
+router.post("/posts", (req, res) => {
+    let accessToken;
+    let imageURl;
+    let message;
+
+    const postUrl = "https://graph.facebook.com/me/photos?published=true&" +
+        "access_token=" + accessToken +
+        "&url=" + imageURl +
+        "&message=" + message;
+    
+    var options = {
+        method: 'POST',
+        uri: postUrl,
+        headers: {
+            "Authorization": 'Bearer EAAJhl2elBEgBAJJ8To57gZA0KR7ZBwvYAXIuSvVB8TTHtLPHZC6JW0kc3T0FwZCgO51ZCk5CZC835AcoPesJLFg1ZC1xzphxyOYReswPDdhCu5ZCAFOWTeVdPQl27fXadz5JeDvvQDzG72nlQLRdbZBhZAHbHOwOx0MIyZBX9io5098xHVBUxFQjzoT',
+        },
+        json: true 
+    };
+     
+    request(options)
+        .then(function (data) {
+           res.status(200).send(data);
+        })
+        .catch(function (err) {
+            res.status(400).send(err);
+        });
+});
+
 
 
 
