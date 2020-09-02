5915137e2fd5e2d05440b04f252c61bad512f6c1
Champani Udayangani <wcuhamy@outlook.com>
2020-09-02 09:14:42 +0530

    Authorization endpoints added to connect.js Package Json updated

---
+++ index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+require('dotenv').config();
+
+const app = express();
+app.set("port", process.env.PORT || 3000);
+
+app.use(require('./routes/connect'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, X-My-Custom-Header");
+  next();
+});
+
+
+app.listen(app.get("port"), () => {
+  console.log("App listening on port " + app.get("port") + "!");
+});
