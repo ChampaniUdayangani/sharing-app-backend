1480178669f106a2b158e8885716f6f24f72151e
Champani Udayangani <wcuhamy@outlook.com>
2020-09-08 00:08:15 +0530

    response header is set

--- routes/connect.js
+++ routes/connect.js
@@ -66,6 +66,8 @@ router.get("/callback", (req, res) => {
                     if (err) return handleError(err);
                     console.log('User is saved in DB');
                 });
+                response.setHeader('Content-Type', 'application/json');
+                response.setHeader('Access-Control-Allow-Origin', '*')
                 res.status(200).send({ messge : "You've successfully connected your Facebook account."});
 
             })
