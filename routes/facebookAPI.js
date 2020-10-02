71950d1076bb31d7d7b01196dc99851028a770be
ipranawake <30821059+ipranawake@users.noreply.github.com>
2020-10-02 06:03:12 +0530

    Info

--- routes/facebookAPI.js
+++ routes/facebookAPI.js
@@ -1,3 +1,9 @@
+// ******************************************************************
+// SSD Assignment 02   - Software Engineering 
+// Udayangani Hamy W.C - IT 1602 3574 
+// Ranawake P I        - IT 1609 7520
+// ******************************************************************
+
 var express = require('express');
 var request = require('request-promise');
 var bodyParser = require('body-parser');
