ac4185267b3f613b076b0d903af6d935aea562da
Priyanke Indunil Ranawake <it16097520@my.sliit.lk>
2020-10-06 00:59:57 +0530

    Readability

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 // SSD Assignment 02   - Software Engineering 
 // Udayangani Hamy W.C - IT 1602 3574 
 // Ranawake P I        - IT 1609 7520
+// 
 // index.js
 // The Entry point of the application
 // ******************************************************************
