03b391fd9c3d77e0cfe8605727595aa63513f93d
Priyanke Indunil Ranawake <it16097520@my.sliit.lk>
2020-10-06 00:51:00 +0530

    Readability

--- index.js
+++ index.js
@@ -3,11 +3,12 @@
 // Udayangani Hamy W.C - IT 1602 3574 
 // Ranawake P I        - IT 1609 7520
 // index.js
+// The Entry point of the application
 // ******************************************************************
 
 var express = require('express');
 const app = express();
-app.set("port", process.env.PORT || 3000); 
+app.set("port", process.env.PORT || 3000); // Set the PORT as 3000
 
 // Routes for authorization & resource access
 app.use(require('./routes/connect'));
@@ -17,7 +18,3 @@ app.use(require('./routes/facebookAPI'));
 app.listen(app.get("port"), () => {
   console.log("App listening on port " + app.get("port") + "!");
 });
-
-
-
-
